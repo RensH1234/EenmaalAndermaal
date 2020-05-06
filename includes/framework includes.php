068269d3674a1533c6a1560e410ee7122367273b
Liander van Bergen <64014656+liandervanbergen@users.noreply.github.com>
2020-05-06 14:20:46 +0200

    Opset nav gebouwd

--- includes/framework includes.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="framework/css/bootstrap.css">
-<script src="framework/js/jQuery.js"></script>
-<script src="framework/js/Popper.js"></script>
-<script src="framework/js/bootstrap.js"></script>
---
+++ includes/frameworkIncludes.php
@@ -0,0 +1,12 @@
+<!--    In dit script zijn de standaard stylesheets en scripts toegevoegd-->
+
+<link rel="stylesheet" href="./framework/css/bootstrap.css">
+
+<script src="../framework/js/jQuery.js"></script>
+<script src="../framework/js/Popper.js"></script>
+<script src="../framework/js/bootstrap.js"></script>
+<script src="../framework/js/bootstrap.bundle.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
