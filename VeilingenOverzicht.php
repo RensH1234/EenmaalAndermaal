b9577db340462fba0fdee775ef4fe0080c1c9b58
RensH1234 <rensharinck@gmail.com>
2020-05-12 12:11:15 +0200

    Prijsranges werken! De css moet nog wel worden gedaan.

--- VeilingenOverzicht.php
+++ VeilingenOverzicht.php
@@ -12,6 +12,23 @@ if (!array_key_exists("zoekopdracht", $_GET)) {
 if (!array_key_exists("filter", $_GET)) {
     $_GET["filter"] = 0;
 }
+$filters = array();
+$resultaten = new Zoekmachine();
+if (array_key_exists("prijsrange1", $_GET)) {
+    array_push($filters,"1");
+}
+if (array_key_exists("prijsrange2", $_GET)) {
+    array_push($filters,"2");
+}
+if (array_key_exists("prijsrange3", $_GET)) {
+    array_push($filters,"3");
+}
+if (array_key_exists("prijsrange4", $_GET)) {
+    array_push($filters,"4");
+}
+$resultaten->prijsfilter(implode(".",$filters));
+$resultaten->_constructNieuw($_GET["zoekopdracht"], $_GET["filter"]);
+$idArray = explode(".", $resultaten->_getIdArrayRes());
 ?>
 
 <!doctype html>
@@ -28,9 +45,10 @@ if (!array_key_exists("filter", $_GET)) {
 <body>
 <?php include_once 'h_test.php';?>
 <main>
-    <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Filter op
+    <div class="dropdown" id="sorteerdropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+            Sorteer op:
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <form action="VeilingenOverzicht.php">
@@ -39,28 +57,42 @@ if (!array_key_exists("filter", $_GET)) {
                 <button type="submit" class="dropdown-item">Prijs van laag naar hoog</button>
             </form>
             <form action="VeilingenOverzicht.php">
-                <input type="hidden" value="<?php echo $_GET["zoekopdracht"];?>" name="zoekopdracht">
+                <input type="hidden" value="<?php echo $_GET["zoekopdracht"]; ?>" name="zoekopdracht">
                 <input type="hidden" value="1" name="filter">
                 <button type="submit" class="dropdown-item">Prijs van hoog naar laag</button>
             </form>
-            <form action="VeilingenOverzicht.php">
-                <input type="hidden" value="<?php echo $_GET["zoekopdracht"];?>" name="zoekopdracht">
-                <input type="hidden" value="2" name="filter">
-                <button type="submit" class="dropdown-item">Afstand</button>
-            </form>
         </div>
     </div>
-    <h2>Resultaten die overeen komen met "<?php echo $_GET["zoekopdracht"];?>"</h2>
-        <?php
-            $resultaten = new Zoekmachine();
-            $resultaten->_constructNieuw($_GET["zoekopdracht"],$_GET["filter"]);
-            $idArray = explode(".",$resultaten->_getIdArrayRes());
-            if($idArray[0]!=null) {
-                $resultaatVeilinglijst = new Veilinglijst();
-                $resultaatVeilinglijst->_construct($idArray, "", "ResultatenLijst");
-                $resultaatVeilinglijst->printVeilingen();
-            }
-            else{
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+            Prijs:
+        </button>
+        <form action="VeilingenOverzicht.php">
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <input type="hidden" value="<?php echo $_GET["zoekopdracht"]; ?>" name="zoekopdracht">
+                <input type="hidden" value="0" name="filter">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" name="prijsrange1" value="1">
+                    <label class="form-check-label" for="inlineCheckbox1">&euro;0 - &euro;10</label><br>
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox2" name="prijsrange2" value="1">
+                    <label class="form-check-label" for="inlineCheckbox2">&euro;10 - &euro;25</label><br>
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox3" name="prijsrange3" value="1">
+                    <label class="form-check-label" for="inlineCheckbox3">&euro;25 - &euro;50</label><br>
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox4" name="prijsrange4" value="1">
+                    <label class="form-check-label" for="inlineCheckbox4">&euro;50+</label><br>
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">Reset filter</button>
+        </form>
+    </div>
+    <h2>Resultaten die overeen komen met <?php echo $_GET["zoekopdracht"]; ?></h2>
+    <?php
+    if ($idArray[0] != null) {
+        $resultaatVeilinglijst = new Veilinglijst();
+        $resultaatVeilinglijst->_construct($idArray, "", "ResultatenLijst");
+        $resultaatVeilinglijst->printVeilingen();
+    } else {
                 echo "<p>Er komen geen producten overeen.</p>";
             }
         ?>
