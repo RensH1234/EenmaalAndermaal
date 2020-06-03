9ea9343a6aacdfe365e8fa8cd61a298af010ffb2
Bas <bfaruijs2@gmail.com>
2020-06-03 13:09:42 +0200

    hot rubrieken en aflopende veilingen

--- Index.php
+++ Index.php
@@ -3,6 +3,7 @@ require_once 'SessionHandling/Session.php';
 
 include_once 'Framework.php';
 include_once 'php classes/Veilinglijst.php';
+include_once 'php classes/Rubriekenlijst.php';
 include_once 'Functions.php';
 $title = 'Eenmaal Andermaal!';
 $siteNaam = 'Welkom!';
@@ -35,6 +36,34 @@ $huidigeJaar = date('Y');
         $lijstComputers = new Veilinglijst();
         $lijstComputers->_maakVeilingen($lijstComputers->_fetchHotveilingen(6));
         echo $lijstComputers->_genereerContainer("Hot Veilingen!");
+
+        $lijstAflopend = new Veilinglijst();
+        $lijstAflopend ->_maakVeilingen($lijstAflopend->_fetchAflopendeveilingen(6));
+        echo $lijstAflopend ->_genereerContainer("Aflopende Veilingen!");
+
+
+        echo "<div class='container'>";
+        $lijstRubrieken = new Rubriekenlijst();
+        $rubriekID= array();
+            $rubriekID = $lijstRubrieken->_fetchHotRubrieken(4);
+        echo "<div class='row mt-4 text-center bg-dark'><div class='col'><h1 class='text-body'>Hot categorieën</h1>";
+        echo "<ul>";
+        echo "<div class=\"list-group\">";
+            for ($j = 0; $j < 4; $j++) {
+                if($rubriekID[$j]!=-1) {
+                    $rubrieknaam = $lijstRubrieken->getRubrieknaam($rubriekID[$j])[0];
+                    $url= "VeilingenOverzicht.php?rubriekID=" . $rubriekID[$j];
+                    echo "<a href=\"";
+                    echo $url;
+                    echo "\" class=\"list-group-item list-group-item-action\">";
+                    echo $rubrieknaam;
+                    echo "</a>";
+                    $rubriekID[$j] = $lijstRubrieken->getSuperRubriek($rubriekID[$j]);
+                }
+                echo "<br>";
+            }
+        echo "</ul>";
+            echo "</div></div>";
         ?>
     </div>
 
